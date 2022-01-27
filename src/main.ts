cca3a8664c32a64245c8bc95ece1d8ad95267eea
zhengqi <178361353@qq.com>
2022-01-27 16:03:29 +0800

    fix: test init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
+/*
+ * @Description: 
+ * @Author: zhengqi
+ * @Date: 2022-01-27 15:49:00
+ * @LastEditTime: 2022-01-27 16:03:05
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//  ttt
 createApp(App).use(store).use(router).mount('#app')
